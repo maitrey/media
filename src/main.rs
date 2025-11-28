f14441205ee726716dd0ae9e2d1d97cad5de325c
Maitrey Mishra <maitreymishra23@gmail.com>
2025-11-28 12:11:12 +0100

    Create main.rs

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+#[derive(Debug)]
+enum Media {
+    Book {title: String, author: String},
+    Movie {title: String, director: String },
+    Audiobook {title: String},
+}
+
+fn print_media(media: Media) {
+    println!("{:?}", media);
+}
+
+fn main() {
+    let audiobook = Media::Audiobook {
+        title: String::from("Media book"),
+    };
+    print_media(audiobook);
+}
